8e2be1c37610f173a36e5ac976da9f3fa8578a52
Andrey Koppel <akoppela@gmail.com>
2019-08-01 23:53:38 -0400

    Removed auto generated part from init.el config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,1 @@
 (org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (evil-commentary use-package spaceline evil-surround evil-org cycle-themes base16-theme auto-compile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
