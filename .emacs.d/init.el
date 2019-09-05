8dc0b08ec3ecca4b495490caad37d86e8b25c683
Andrey Koppel <akoppela@gmail.com>
2019-09-05 06:34:13 -0400

    Added treemacs to emacs config.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,10 +19,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(evil-collection-setup-minibuffer t)
  '(package-selected-packages
    (quote
-    (wgrep web-mode json-mode vlf js2-mode elm-mode multiple-cursors elisp-format counsel-projectile emmet-mode ag symon evil-magit magit which-key use-package spaceline evil-surround evil-org evil-commentary cycle-themes base16-theme auto-compile))))
+    (elfeed-org elfeed benchmark-init helpful which-key json-mode js2-mode flycheck-elm elm-mode emmet-mode web-mode org-bullets flycheck company evil-magit magit counsel-projectile treemacs-projectile treemacs-evil treemacs wgrep multiple-cursors counsel general evil-commentary evil-org evil-surround evil-collection evil rainbow-mode centered-cursor-mode spaceline base16-theme vlf auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
