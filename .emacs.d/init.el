8201832aaf19557d0ac8b46396103ddbc58a6954
Andrey Koppel <akoppela@gmail.com>
2019-09-09 08:07:16 -0400

    Used custom instead of config in emacs configuration.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,6 +20,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(evil-collection-setup-minibuffer t)
+ '(base16-theme-256-color-source (quote colors))
  '(package-selected-packages
    (quote
     (iedit elfeed-org elfeed benchmark-init helpful which-key json-mode js2-mode flycheck-elm elm-mode emmet-mode web-mode org-bullets flycheck company evil-magit magit counsel-projectile treemacs-projectile treemacs-evil treemacs wgrep multiple-cursors counsel general evil-commentary evil-org evil-surround evil-collection evil rainbow-mode centered-cursor-mode spaceline base16-theme vlf auto-compile use-package))))
