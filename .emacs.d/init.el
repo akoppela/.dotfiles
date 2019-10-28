9fbbc2108bf9c3bf9dd07d286c9a991ad501513c
Andrey Koppel <akoppela@gmail.com>
2019-10-28 17:37:45 +0800

    Emacs: Used init instead of custom for packages.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,8 +19,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(base16-theme-256-color-source (quote colors))
- '(evil-collection-setup-minibuffer t)
  '(package-selected-packages
    (quote
     (save-visited-files iedit elfeed-org elfeed benchmark-init helpful which-key json-mode js2-mode flycheck-elm elm-mode emmet-mode web-mode org-bullets flycheck company evil-magit magit counsel-projectile treemacs-projectile treemacs-evil treemacs wgrep multiple-cursors counsel general evil-commentary evil-org evil-surround evil-collection evil rainbow-mode centered-cursor-mode spaceline base16-theme vlf auto-compile use-package)))
