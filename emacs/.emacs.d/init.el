fa3fff6d8deb782a7b3ef4d8902f864a26eb7c53
Andrey Koppel <akoppela@gmail.com>
2019-11-25 20:21:38 +0800

    Emacs: Added magit forge.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,10 +19,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (guix eslint-fix add-node-modules-path avy-menu expand-region evil-anzu save-visited-files iedit elfeed-org elfeed benchmark-init helpful which-key json-mode js2-mode flycheck-elm elm-mode emmet-mode web-mode org-bullets flycheck company evil-magit magit counsel-projectile treemacs-projectile treemacs-evil treemacs wgrep multiple-cursors counsel general evil-commentary evil-org evil-surround evil-collection evil rainbow-mode centered-cursor-mode spaceline base16-theme vlf auto-compile use-package)))
+    (guix forge eslint-fix add-node-modules-path avy-menu expand-region evil-anzu save-visited-files iedit elfeed-org elfeed benchmark-init helpful which-key json-mode js2-mode flycheck-elm elm-mode emmet-mode web-mode org-bullets flycheck company evil-magit magit counsel-projectile treemacs-projectile treemacs-evil treemacs wgrep multiple-cursors counsel general evil-commentary evil-org evil-surround evil-collection evil rainbow-mode centered-cursor-mode spaceline base16-theme vlf auto-compile use-package)))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
