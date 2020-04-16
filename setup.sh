e6cfe0601115cbdd063487bb939ec6e3ad11b69b
Andrey Koppel <akoppela@gmail.com>
2020-04-16 17:31:17 +0800

    Shared: Improved setup.sh script.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #! /bin/sh
 
+# Cleanup
+rm ~/.bashrc
+
+# Create
+touch ~/.secret-profile
+
 # Stow
 cd ~/.dotfiles && stow bash
 cd ~/.dotfiles && stow emacs
 cd ~/.dotfiles && stow git
+
+# Source profile
+. ~/.bashrc
