08ce3407dfb242005af8d10504692ce006c74e34
akoppela <akoppela@gmail.com>
2021-09-14 10:21:12 +0200

    Nix: Fix WireGuard comments.

--- nix/wireguard.nix
+++ nix/wireguard.nix
@@ -6,7 +6,7 @@ let
   port = 51820;
 in
 {
-  # enable NAT
+  # Enable NAT
   networking.nat.enable = true;
   networking.nat.externalInterface = externalInterface;
   networking.nat.internalInterfaces = [ internalInterface ];
@@ -23,7 +23,7 @@ in
       # The port that WireGuard listens to. Must be accessible by the client.
       listenPort = port;
 
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      # This allows the WireGuard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
       postSetup = ''
         ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ${externalInterface} -j MASQUERADE
