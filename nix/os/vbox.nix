fde1f368cd1604e0277142cc4687462b09824c0f
akoppela <akoppela@gmail.com>
2021-10-06 00:35:37 +0300

    Nix: Enable X server correctly for VBox.

--- nix/os/vbox.nix
+++ nix/os/vbox.nix
@@ -13,8 +13,8 @@
   networking.hostName = "vbox-nixos";
   time.timeZone = "Europe/Moscow";
 
-  # Enable GUI
-  akoppela.enableX = true;
+  # Enable X server
+  services.xserver.enable = true;
 
   # FIXME: UUID detection is currently broken
   boot.loader.grub.fsIdentifier = "provided";
