a96a768a31701bad109f946dacbbd3f3cde85e69
akoppela <akoppela@gmail.com>
2022-01-04 12:24:06 +0100

    Nix: Nano: Remove TailScale.

--- nix/os/nano/networking.nix
+++ nix/os/nano/networking.nix
@@ -11,7 +11,4 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   networking.useDHCP = false;
   networking.interfaces.wlp0s20f3.useDHCP = true;
-
-  # Enable Tailscale
-  services.tailscale.enable = true;
 }
