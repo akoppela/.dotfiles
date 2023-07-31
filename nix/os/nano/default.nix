5d313b7a14ada1c5770a27ff2646b00271bd835a
akoppela <akoppela@gmail.com>
2023-07-31 17:51:40 +0700

    Nix: Nano: Enable fstrim.

--- nix/os/nano/default.nix
+++ nix/os/nano/default.nix
@@ -18,6 +18,9 @@
     dpi = 165;
   };
 
+  # Enable SSD trim
+  services.fstrim.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
