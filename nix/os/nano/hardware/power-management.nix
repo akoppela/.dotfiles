735a1cd271ff07f76d45f9d15f0fbc7927b7cb03
akoppela <akoppela@gmail.com>
2024-01-04 15:46:44 +0700

    Nix: Nano: Update TLP settings.

--- nix/os/nano/hardware/power-management.nix
+++ nix/os/nano/hardware/power-management.nix
@@ -6,6 +6,28 @@
   boot.extraModulePackages = [ config.boot.kernelPackages.acpi_call ];
 
   # Enable power management
-  services.tlp.enable = true;
-  powerManagement.cpuFreqGovernor = "powersave";
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_DRIVER_OPMODE_ON_AC = "active";
+      CPU_DRIVER_OPMODE_ON_BAT = "active";
+
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 30;
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+    };
+  };
 }
