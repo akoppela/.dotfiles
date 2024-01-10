335a750499d11e397ea9cc6c72ea897096b69fb2
akoppela <akoppela@gmail.com>
2024-01-10 18:10:53 +0700

    Nix: Disable GDM.

--- nix/module/xserver.nix
+++ nix/module/xserver.nix
@@ -7,7 +7,6 @@
     libinput.enable = true;
     libinput.touchpad.naturalScrolling = true;
     displayManager.startx.enable = true;
-    displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
 }
