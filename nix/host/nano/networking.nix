5121e44b6277d8f5cb2e0f71daa54bdc48a0407f
akoppela <akoppela@gmail.com>
2024-01-17 22:23:32 +0700

    Nix: Remove comments.

--- nix/host/nano/networking.nix
+++ nix/host/nano/networking.nix
@@ -43,13 +43,9 @@ in
     privateKeyFile = "/root/.config/wireguard/private";
     peers = [
       {
-        # Public key of the server
         publicKey = "b+eJgKELR8XcZeGIBcviLDMAA1eatJutD6wVP3rUN2c=";
-        # Forward all the traffic via VPN
         allowedIPs = [ "0.0.0.0/0" ];
-        # Set this to the server IP and port
         endpoint = "128.199.16.170:51820";
-        # Send keepalives every 25 seconds, important to keep NAT tables alive
         persistentKeepalive = 25;
       }
     ];
