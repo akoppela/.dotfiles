f9432c013c5e126ae8527e06278fdff1e39c943f
akoppela <akoppela@gmail.com>
2024-01-25 00:30:30 +0700

    Nix: Set correct DPI for P16s.

--- nix/host/p16s/default.nix
+++ nix/host/p16s/default.nix
@@ -22,6 +22,8 @@
     "zoom"
   ];
 
+  services.xserver.dpi = 141;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
