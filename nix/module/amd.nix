249aaa252668cf96e09d1bc06c24af6a0074a3d6
akoppela <akoppela@gmail.com>
2024-01-24 23:21:42 +0700

    Nix: Add AMD GPU support.

--- nix/module/amd.nix
+++ nix/module/amd.nix
@@ -1,5 +1,12 @@
+{ pkgs, ... }:
+
 {
   boot.kernelModules = [ "kvm-amd" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
   hardware.cpu.amd.updateMicrocode = true;
+
+  hardware.opengl.extraPackages = [ pkgs.amdvlk ];
 }
