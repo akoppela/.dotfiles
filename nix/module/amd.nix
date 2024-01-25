e3b4747ee603d5dc104801c25b51636e92807294
akoppela <akoppela@gmail.com>
2024-01-25 18:25:05 +0700

    Nix: Remove unnecessary AMD settings.

--- nix/module/amd.nix
+++ nix/module/amd.nix
@@ -1,12 +1,6 @@
-{ pkgs, ... }:
-
 {
   boot.kernelModules = [ "kvm-amd" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
 
-  services.xserver.videoDrivers = [ "amdgpu" ];
-
   hardware.cpu.amd.updateMicrocode = true;
-
-  hardware.opengl.extraPackages = [ pkgs.amdvlk ];
 }
