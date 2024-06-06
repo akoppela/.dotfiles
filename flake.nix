a9834d43750f20d1bf07093c4ca4db6d34882b62
akoppela <akoppela@gmail.com>
2024-06-06 11:26:06 +0700

    Nix: Add unstable packages.

--- flake.nix
+++ flake.nix
@@ -3,37 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, nixpkgsUnstable, home-manager, ... }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-      ];
+      pkgs = import nixpkgs { inherit system; };
+      pkgsUnstable = import nixpkgsUnstable { inherit system; };
+      system = "x86_64-linux";
     in
     {
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.nixpkgs-fmt
-              pkgs.nixops_unstable
-            ];
-          };
-        }
-      );
+      devShells."${system}".default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nixpkgs-fmt
+          pkgs.nixops_unstable
+        ];
+      };
 
       nixosConfigurations = {
         nano = nixpkgs.lib.nixosSystem {
           specialArgs = {
-            inherit home-manager;
+            inherit home-manager pkgsUnstable;
           };
 
           modules = [
@@ -43,7 +37,7 @@
 
         p16s = nixpkgs.lib.nixosSystem {
           specialArgs = {
-            inherit home-manager;
+            inherit home-manager pkgsUnstable;
           };
 
           modules = [
